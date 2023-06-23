5a2974ae73c31416cf526e92d82a0e4e61187989
Tina Tsai <tinatsai0122@hotmail.com>
2023-06-23 16:33:14 +0200

    adding DOM exer add item and table ex

--- DOM/demo1/script.js
+++ DOM/demo1/script.js
@@ -32,12 +32,69 @@
 //  console.log("*************************")
 
  
-let newLi = document.createElement("li")
-// ajouter une valeur a l'interieur
-newLi.innerHTML = "Salade"
-//recupere ma liste
+// let newLi = document.createElement("li")
+// // ajouter une valeur a l'interieur
+// newLi.innerHTML = "Salade"
+// //recupere ma liste
+// let maListe = document.getElementById("maListe")
+
+// maListe.appendChild(newLi)
+
+// console.log(newLi)
+
+// //#region insert before
+// let newLi = document.createElement("li")
+
+// //je lui ajoute une valeur a l'interieur
+// newLi.innerHTML="InsertBefore"
+
+// //Je recupere ma lastItem
+// let maListe = document.getElementById("maListe")
+// let lastItem = document.getElementById("lastItem")
+
+// //J'ajoute un element avant
+// maListe.insertBefore(newLi,lastItem)
+
+// //#endregion
+
+// //#region insertAdjacentHTML
+
+// let maListe = document.getElementById("maListe")
+// maListe.insertAdjacentHTML("beforebegin","<h1>beforebegin</h1>")
+// maListe.insertAdjacentHTML("afterbegin","<li>afterbegin</li>")
+// maListe.insertAdjacentHTML("beforeend","<li>beforeend</li>")
+// maListe.insertAdjacentHTML("afterend","<h1>afterend</h1>")
+// //#endregion
+
+//#region Replace Child
+
+// //Creation du nouvel element
+// let newElement = document.createElement("li")
+// newElement.innerHTML = "newElement"
+
+// //recuperation de la list
+// let maListe = document.getElementById("maListe")
+
+// //recupaeration du noeud a modifier
+// let elementToUpdate = maListe.childNodes[1]
+
+// //remplacement du noeud existant par le nouveau
+// maListe.replaceChild(newElement, elementToUpdate)
+
+// //#endregion
+
+//#region Remove Child
+
+//recuperation de la list
 let maListe = document.getElementById("maListe")
 
-maListe.appendChild(newLi)
+//recupaeration du noeud a modifier
+let elementToRemove = maListe.childNodes[1]
 
-console.log(newLi)
+//remove
+maListe.removeChild(elementToRemove)
+
+//#endregion
+
+//#region
+
